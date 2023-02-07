b0a9bc75029eb609776e32fd53d0d55bded2757a
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-08 00:41:59 +0530

    Update WithISP.java

--- WithISP.java
+++ WithISP.java
@@ -24,7 +24,6 @@ class Daniel implements Teacher{
   @Override
   void teachDBMS(){
         System.out.println("Teaching DBMS");
-
   }
 }
 
@@ -32,7 +31,6 @@ class Alex implements Teacher{
   @Override
   void teachOS(){
         System.out.println("Teaching OS");
-
   }
 }
 
@@ -40,9 +38,5 @@ class Joe implements Teacher{
   @Override
   void teachCN(){
         System.out.println("Teaching CN");
-
   }
-}
-
-  
 }
