5e86aec0168b88bae3fe0405545f5146763b8b4d
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-07 22:11:50 +0530

    Update and rename SRP.java to WithoutSRP.java

---
+++ WithoutSRP.java
@@ -0,0 +1,27 @@
+class Facebook{
+  public CreatePostResponse createPost(CreatePostRequest createPostRequest){
+    // create post logic
+  }
+  
+  public DeletePostResponse deletePost(DeletePostRequest deletePostRequest){
+    // delete post logic
+  }
+  
+  public LikePostResponse likePost(LikePostRequest likePostRequest){
+    // like post logic
+  }
+  
+  public DislikePostResponse dislikePost(DislikePostRequest dislikePostRequest){
+    // dislike post logic
+  }
+  
+  public CreateCommentResponse createComment(CreateCommentRequest createCommentRequest){
+    // create comment logic
+  }
+  
+  public DeleteCommentResponse deleteComment(DeleteCommentRequest deleteCommentRequest){
+    // delete comment logic
+  }
+}
+
+
