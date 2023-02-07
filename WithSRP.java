b7c4e8b00940aaf7f74f1785ebb212bb0ef1c7bc
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-07 22:15:02 +0530

    Update WithSRP.java

--- WithSRP.java
+++ WithSRP.java
@@ -1,1 +1,29 @@
-/
+class PostFeature{
+  public CreatePostResponse createPost(CreatePostRequest createPostRequest){
+    // create post logic
+  }
+  
+  public DeletePostResponse deletePost(DeletePostRequest deletePostRequest){
+    // delete post logic
+  }
+}
+
+class LikesFeature{
+  public LikePostResponse likePost(LikePostRequest likePostRequest){
+    // like post logic
+  }
+  
+  public DislikePostResponse dislikePost(DislikePostRequest dislikePostRequest){
+    // dislike post logic
+  }
+}
+
+class CommentsFeature{
+  public CreateCommentResponse createComment(CreateCommentRequest createCommentRequest){
+    // create comment logic
+  }
+  
+  public DeleteCommentResponse deleteComment(DeleteCommentRequest deleteCommentRequest){
+    // delete comment logic
+  }
+}
