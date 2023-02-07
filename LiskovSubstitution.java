9ec0feac95d2c805105df2a50efa353bb5ba0876
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-07 23:55:38 +0530

    Update LiskovSubstitution.java

--- LiskovSubstitution.java
+++ LiskovSubstitution.java
@@ -1,1 +1,18 @@
 
+class Polygon{
+  public int findArea(int dimension){
+    // some logic to find area
+  }
+}
+
+class Square extends Polygon{
+  public int findArea(int dimension){
+    return dimension * dimension;
+  }
+}
+
+class WrongSquare extends Polygon{
+  public int findArea(int dimension){
+    return dimension;
+  }
+}
