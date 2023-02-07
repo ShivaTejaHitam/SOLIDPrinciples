074f6269a93b33569b5f60966c75daed76acacb9
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-08 00:41:23 +0530

    Create WithISP.java

---
+++ WithISP.java
@@ -0,0 +1,48 @@
+public interface Teacher {
+
+    void teachJava();
+
+    void teachDBMS();
+
+    void teachOS();
+    
+    void teachCN();
+
+}
+
+
+
+class John implements Teacher{
+  
+  @Override 
+  void teachJava(){
+    System.out.println("Teaching Java");
+  }
+}
+
+class Daniel implements Teacher{
+  @Override
+  void teachDBMS(){
+        System.out.println("Teaching DBMS");
+
+  }
+}
+
+class Alex implements Teacher{
+  @Override
+  void teachOS(){
+        System.out.println("Teaching OS");
+
+  }
+}
+
+class Joe implements Teacher{
+  @Override
+  void teachCN(){
+        System.out.println("Teaching CN");
+
+  }
+}
+
+  
+}
