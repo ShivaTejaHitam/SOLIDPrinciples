55d9ee2d63e412c85e4ea3a81152173efbe135ee
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-08 13:49:21 +0530

    Create DependencyInversionPrinciple.java

---
+++ DependencyInversionPrinciple.java
@@ -0,0 +1,17 @@
+
+class Computer{
+  
+  private AcerKeyboard acerKeyboard;
+  private DellMouse dellMouse;
+  
+  Computer(AcerKeyboard acerKeyboard,DellMouse dellMouse){
+    this.acerKeyboard = acerKeyboard;
+    this.dellMouse = dellMouse;
+  }
+}
+
+class AcerKeyboard{
+}
+
+class DellMouse{
+}
