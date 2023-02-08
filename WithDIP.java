a12914d05a4bbd26c1e2f20d5b5321b7fa305f08
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-02-08 13:55:00 +0530

    Create WithDIP.java

---
+++ WithDIP.java
@@ -0,0 +1,30 @@
+public interface Mouse{
+}
+
+public interface Keyboard{
+}
+
+class Computer{
+  
+  private Keyboard keyboard;
+  private Mouse mouse;
+  
+  Computer(Keyboard keyboard,Mouse mouse){
+    this.keyboard = keyboard;
+    this.mouse = mouse;
+  }
+}
+
+class AcerKeyboard implements Keyboard{
+}
+
+class DellMouse implements Mouse{
+}
+
+
+class Main{
+  Keyboard keyboard = new AcerKeyboard();
+  Mouse mouse = new DellMouse();
+  Computer  computer  = new Computer(keyboard,mouse);
+}
+
